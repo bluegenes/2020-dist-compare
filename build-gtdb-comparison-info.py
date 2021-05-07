6a9724b0a62bd726a10c6481c30a63a4bd43d78a
N Tessa Pierce <ntpierce@gmail.com>
2021-05-07 16:45:00 -0700

    test functions in build-gtdb-comparisons

--- build-gtdb-comparison-info.py
+++ build-gtdb-comparison-info.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import glob
 import pprint
+import pytest
 
 import pandas as pd
 from sourmash.lca import lca_utils
@@ -11,6 +12,19 @@ from itertools import combinations
 
 ComparisonInfo = namedtuple('ComparisonInfo',
                           'comparison_name, accessionA, accessionB, lowest_common_rank, lowest_common_lineage, is_representativeA, is_representativeB')
+
+
+def find_lca(linA,linB, reverse_taxlist=["species", "genus", "family", "order", "class", "phylum", "superkingdom"]):
+    lca_rank=None
+    lca_lin=None
+    for rank in reverse_taxlist:
+            # do we have a lca lineage match at this rank?
+        if lca_utils.is_lineage_match(linA, linB, rank):
+            lca_rank = rank
+            lca_lin = lca_utils.pop_to_rank(linA, lca_rank)
+            break
+    return lca_rank, lca_lin
+
 
 
 def main(args):
@@ -26,9 +40,10 @@ def main(args):
     cInfo = []
     # get dictionary of species_path :: accession
     acc2path = pd.Series(metadataDF[taxonomy].values,index=metadataDF.index).to_dict()
+    reverse_taxlist = list(lca_utils.taxlist(include_strain=False))[::-1]
     # iterate through all possible pairwise comparisons
     for n, (acc1, acc2) in enumerate(combinations(acc2path.keys(), r = 2)):
-        comparison_name = f"{acc1}_{acc2}"
+        comparison_name = f"{acc1}_x_{acc2}"
         if n !=0 and n % 10000 == 0:
             print(f"... checking {n}th comparison, {comparison_name}\n")
         # check if either are gtdb representative genomes
@@ -37,19 +52,12 @@ def main(args):
           rep1 = True
         if acc2 in representative_accs:
           rep2 = True
-        path1 = lca_utils.make_lineage(acc2path[acc1])
-        path2 = lca_utils.make_lineage(acc2path[acc2])
-        lca_rank = None
-        # for rank in reverse taxlist (species --> superk)
-        for rank in list(lca_utils.taxlist(include_strain=False))[::1]:
-            # do we have a lca lineage match at this rank?
-            if lca_utils.is_lineage_match(path1, path2, rank):
-                lca_rank = rank
-                break
+        lin1 = lca_utils.make_lineage(acc2path[acc1])
+        lin2 = lca_utils.make_lineage(acc2path[acc2])
+        lca_rank,lca_lin = find_lca(lin1,lin2)
        # if there's an LCA rank, store the comparison
         if lca_rank:
-            lca_lin = lca_utils.pop_to_rank(path1, lca_rank)
-            lca_lin = ";".join(lca_utils.zip_lineage(lca_lin))
+            lca_lin = ";".join(lca_utils.zip_lineage(lca_lin, truncate_empty=True))
             if n !=0 and n % 1000 == 0:
                 print(f"...adding lca comparison for rank {lca_rank}: {comparison_name}")
             cInfo.append(ComparisonInfo(comparison_name,acc1,acc2,lca_rank,lca_lin,rep1,rep2))
@@ -73,4 +81,46 @@ def cmdline(sys_args):
 if __name__ == '__main__':
     returncode = cmdline(sys.argv[1:])
     sys.exit(returncode)
+
+
+def test_build_comparison_lowest_lca():
+    lin1 = lca_utils.make_lineage('a;b;c')
+    print(lin1)
+    lin2 = lca_utils.make_lineage('a;b;c')
+
+    lca_rank, lca_lin = find_lca(lin1,lin2)
+    print("LCA RANK", lca_rank)
+    print("LCA LIN", lca_lin)
+    assert lca_rank == "class"
+    assert lca_lin == lin1
+
+def test_build_comparison_mid_lca():
+    lin1 = lca_utils.make_lineage('a;b;c')
+    lin2 = lca_utils.make_lineage('a;b;d')
+    true_lca_lin = lca_utils.make_lineage('a;b')
+
+    lca_rank, lca_lin = find_lca(lin1,lin2)
+    assert lca_rank == "phylum"
+    assert lca_lin == true_lca_lin
+
+def test_build_comparison_highest_lca():
+    lin1 = lca_utils.make_lineage('a;b;c')
+    lin2 = lca_utils.make_lineage('a;d;e')
+    true_lca_lin = lca_utils.make_lineage('a')
+
+    lca_rank, lca_lin = find_lca(lin1,lin2)
+    assert lca_rank == "superkingdom"
+    assert lca_lin == true_lca_lin
+
+def test_build_comparison_no_lca():
+    lin1 = lca_utils.make_lineage('a;b;c')
+    lin2 = lca_utils.make_lineage('d;e;f')
+
+    lca_rank, lca_lin = find_lca(lin1,lin2)
+    assert lca_rank == None
+    assert lca_lin == None
+
+def test_reverse_taxlist():
+    reverse_taxlist = list(lca_utils.taxlist(include_strain=False))[::-1]
+    assert reverse_taxlist == ["species", "genus", "family", "order", "class", "phylum", "superkingdom"]
 
